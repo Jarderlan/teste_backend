3d11dd64135450d6b5e361605a0124360f6699c0
Erlan Softbuilder <erlan@softbuilder.com.br>
2021-10-09 21:37:16 -0300

    feat: SingInController, falta passar metodos para um service e coreController

--- src/controllers/SingInController.ts
+++ src/controllers/SingInController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, Router } from 'express'
-import { SingIn } from '../types/entities'
+import Enderecos from '../models/Enderecos'
+import Usuarios from '../models/Usuarios'
+import { ISingIn } from '../types/entities'
 const router = Router()
 
 router.get('/teste', async (req: Request, res: Response) => {
@@ -7,8 +9,29 @@ router.get('/teste', async (req: Request, res: Response) => {
 });
 
 router.post('/sing-in', async (req: Request, res: Response) => {
-    const { usuario }: SingIn = req.body
-    res.status(200).json(usuario);
+    const { usuario }: ISingIn = req.body
+    try {
+        const endereco = await Enderecos.query().insert({
+            cep: usuario.endereco.cep,
+            logradouro: usuario.endereco.logradouro,
+            bairro: usuario.endereco.bairro,
+            localidade: usuario.endereco.localidade,
+            uf: usuario.endereco.uf,
+            numero: usuario.endereco.numero,
+            complemento: usuario.endereco.complemento,
+            ibge: usuario.endereco.ibge
+        })
+
+        const usuariosI = await Usuarios.query().insert({
+            ...usuario,
+            endereco_id: endereco.id,
+        })
+        res.status(200).json(usuariosI);
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error);
+    }
+
 });
 
 export default router
