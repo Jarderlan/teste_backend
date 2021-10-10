8911e1c2f2a62be0ff6fc4b5e6746e5fca81c369
Erlan Softbuilder <erlan@softbuilder.com.br>
2021-10-09 21:31:48 -0300

    feat: types CoreController

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+import { Response } from "express";
+
+export interface ISendResponse {
+    data?: object | Array<any>;
+    code: number;
+    res: Response;
+}
+
+export interface IErrorHandlerResponse {
+    message?: string | Array<string>;
+    values?: object;
+    code?: number;
+    res: Response;
+    error: any;
+}
