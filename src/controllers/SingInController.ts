2b9341f38a76e9381dc7020f2798db0abb23b1dc
Erlan Softbuilder <erlan@softbuilder.com.br>
2021-10-11 17:49:12 -0300

    feat: finalizando SingIn

--- src/controllers/SingInController.ts
+++ src/controllers/SingInController.ts
@@ -1,27 +1,25 @@
 import { Request, Response, Router } from 'express'
+import { knex } from '../config/database'
 import { NovoEndereco } from '../services/EnderecoService'
 import { NovoUsuario } from '../services/UsuarioService'
 import { ISingIn } from '../types/entities'
 import { errorHandler, sendResponse } from './CoreController'
-const router = Router()
 
-router.get('/teste', async (req: Request, res: Response) => {
-    res.status(200).json('testando 123');
-});
+const router = Router()
 
 router.post('/sing-in', async (req: Request, res: Response) => {
     const { usuario }: ISingIn = req.body
     try {
-        const endereco = await NovoEndereco(usuario.endereco)
+        await knex.transaction(async trx => {
+            const endereco = await NovoEndereco(usuario.endereco, trx);
 
-        const usuariosI = await NovoUsuario({
-            ...usuario,
-            endereco_id: endereco.id,
-        })
-        sendResponse({
-            data: usuariosI,
-            code: 200,
-            res
+            const usuariosI = await NovoUsuario({ ...usuario, endereco_id: endereco.id, }, trx);
+
+            sendResponse({
+                data: usuariosI,
+                code: 200,
+                res
+            })
         })
     } catch (error) {
         errorHandler({ error, res })
