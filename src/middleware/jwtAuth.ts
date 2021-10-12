d8ecb31ccc5a7d4a3b2c7aa7bdfec8458bd0e699
Erlan Softbuilder <erlan@softbuilder.com.br>
2021-10-12 07:55:27 -0300

    feat: criando middleware para rotas autenticadas

---
+++ src/middleware/jwtAuth.ts
@@ -0,0 +1,18 @@
+import { errorHandler } from "../controllers/CoreController";
+import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+require('dotenv').config()
+
+const Secret = process.env.JWT_SECRET
+
+export async function jwtAuthMiddleware(req: Request, res: Response, next) {
+    const token = req.headers['x-access-token']
+    jwt.verify(token, Secret, (error, decoded) => {
+        if (error) {
+            res.status(400).json(error)
+        }
+        req['user_id'] = decoded.user_id
+        next();
+    })
+
+}
